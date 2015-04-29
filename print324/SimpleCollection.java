9b9d77e1e9667f3a71d14d827229ae705cf3688b
Trololoshe4ki <vladimir4862@gmail.com>
2015-04-29 12:07:34 +0400

    print329

--- print324/SimpleCollection.java
+++ print324/SimpleCollection.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class SimpleCollection {
 	public static void main (String[] args) {
-		Collection<Integer> c = new ArrayList<Integer>();
+		Set<Integer> c = new HashSet<Integer>();	
 		for(int i = 0; i < 10; i++)
 			c.add(i);
 		for(Integer i : c)
